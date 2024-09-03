2ce777b6981bde8d32e30bb028b402c02069abc7
manelly67 <manellylopezqqq@gmail.com>
2024-09-03 14:29:07 -0500

    adding main content

--- src/homepage.js
+++ src/homepage.js
@@ -2,9 +2,10 @@ import imageSmall from './assets/img/photo-small-size.jpg';
 import imageMedium from './assets/img/photo-medium-size.jpg';
 import imageLarge from './assets/img/photo-large-size.jpg';
 import changeTheme from './assets/img/theme-light-dark.png';
+import externalLink from './assets/img/open-in-new.png';
 import { textContentAboutMe } from './assets/text-content';
+import { arrayProjects } from './assets/text-content';
 import { github,linkedin } from './assets/icons-source';
-
 
 function homepage(){
 
@@ -33,7 +34,6 @@ function setTheme() {
     root.className = newTheme;
   }
 
-
 // background colors
 const bgDivFirst = document.createElement('div');
 bgDivFirst.classList.add('bgDivFirst');
@@ -43,8 +43,7 @@ bgDivSecond.classList.add('bgDivSecond');
 const pageContainer = document.createElement('div');
 pageContainer.classList.add('page-container');
 
-
-// header
+// HEADER
 const header = document.createElement('header');
 const textInPhoto = document.createElement('H1');
 textInPhoto.textContent = 'My Portfolio';
@@ -67,7 +66,6 @@ mySmallImg.src = imageSmall;
 photoImg.setAttribute('src', mySmallImg.src);
 photoImg.setAttribute('alt','');
 photoImg.classList.add('photoImg');
-
 
 const aboutMe = document.createElement('section');
 const aboutMeHeading = document.createElement('H2');
@@ -97,24 +95,92 @@ linkedinIcon.setAttribute('src', linkedinImg.src);
 linkedinIcon.setAttribute('alt','Linkedin');
 linkedinIcon.classList.add('iconImg');
 
-// projects content
+// PROJECTS CONTENT
 const main = document.createElement('main');
 const contentHeading = document.createElement('H2');
 const content = document.createElement('section');
 content.classList.add('contentProject');
 
 contentHeading.textContent = 'My work';
-const amountOfProjects = 6;
-for (let i = 0; i < amountOfProjects; i++) {
+for (let i = 0; i < arrayProjects.length; i++) {
     const project = document.createElement('article');
     project.setAttribute('id', `project${i+1}`);
     content.appendChild(project);
 }
 
+const allProjectsDiv = content.querySelectorAll('article');
+const projectsInDiv = [...allProjectsDiv];
+const iterator = projectsInDiv.entries();
 
+projectsInDiv.forEach(() => {
+    let index = iterator.next().value;
+    let x = index[1];
+    console.log(x.getAttribute('id'));
+    const div1 = document.createElement('div');
+    const img = document.createElement('img');
+    const screenshot = new Image();
+    img.setAttribute('src', mySmallImg.src);
+    img.setAttribute('alt','');
+    div1.classList.add('divImgArticle');
+    img.classList.add('photoImg');
+    const div2 = document.createElement('div');
+    const name = document.createElement('h3');
+    const links = document.createElement('div');
+    div2.classList.add('divNameArticle');
+    name.setAttribute('style','grid-column:1/2');
+    links.setAttribute('style','grid-column:2/3');
+    const githubLink = document.createElement('a');
+    githubLink.setAttribute('target','_blank');
+    const githubIcon = document.createElement('img');
+    const githubImg = new Image();
+    githubImg.src = github;
+    githubIcon.classList.add('iconImg');
+    githubIcon.setAttribute('src',githubImg.src);
+    githubIcon.setAttribute('alt','github');
+    const newWindowLink = document.createElement('a');
+    newWindowLink.setAttribute('target','_blank');
+    const newWindowIcon = document.createElement('img');
+    const newWindowImg = new Image();
+    newWindowImg.src = externalLink;
+    newWindowIcon.classList.add('iconImg');
+    newWindowIcon.setAttribute('src',newWindowImg.src);
+    newWindowIcon.setAttribute('alt','new-window');
 
+    const div3 = document.createElement('div');
+    div3.classList.add('divDescriptionArticle');
+    const description = document.createElement('p');
 
-// footer
+    arrayProjects.map((e)=>{
+        switch(e.id===x.getAttribute('id')){
+            case true:
+            screenshot.src=e.screenshotProjectSource;
+            img.setAttribute('src', screenshot.src);
+            img.setAttribute('alt',`screenshot ${e.id}`);
+            img.classList.add('projectImg');
+            name.textContent= e.projectName;
+            githubLink.setAttribute('href',e.githubHref);
+            newWindowLink.setAttribute('href',e.projectNewWindow);
+            description.textContent= e.projectDescription;
+            break;
+            case false:
+            break;
+        }
+    });
+
+    x.appendChild(div1);
+    x.appendChild(div2);
+    x.appendChild(div3);
+    div1.appendChild(img);
+    div2.appendChild(name);
+    div2.appendChild(links);
+    links.appendChild(githubLink);
+    githubLink.appendChild(githubIcon);
+    links.appendChild(newWindowLink);
+    newWindowLink.appendChild(newWindowIcon);
+    div3.appendChild(description);
+})
+
+// FOOTER
 const footer = document.createElement('footer');
 
 
